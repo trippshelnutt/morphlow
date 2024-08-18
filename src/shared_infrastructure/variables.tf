8d2a3a562f653495bf3653ac9cff3e886226fdf9
Tripp Shelnutt <trippshelnutt@outlook.com>
2024-08-17 21:28:25 -0400

    fixup workflow

--- src/shared_infrastructure/variables.tf
+++ src/shared_infrastructure/variables.tf
@@ -1,10 +1,13 @@
+variable "env" {
+  description = "The environment for the deployment"
+  type        = string
+}
+
 variable "region" {
   description = "The region where environment is going to be deployed"
   type        = string
   default     = "us-east-1"
 }
-
-# VPC variables
 
 variable "vpc_cidr" {
   description = "CIDR range for VPC"
