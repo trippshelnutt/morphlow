eaa48ffe49dad54099837ad9dedc498702391f79
Tripp Shelnutt <trippshelnutt@outlook.com>
2024-08-19 21:27:08 -0400

    add client docker

---
+++ src/client/vite.docker.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [plugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+})
