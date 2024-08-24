eb9686263d689521a95c4088b2bf630fd75e1ba5
Tripp Shelnutt <trippshelnutt@outlook.com>
2024-08-24 18:30:42 -0400

    added container repo

---
+++ src/shared_infrastructure/container_repository.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = "morphlow_containers"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "morphlow_container_repository"
+    Project     = "morphlow"
+  }
+}
--- src/shared_infrastructure/main.tf
+++ src/shared_infrastructure/main.tf
@@ -15,31 +15,6 @@ locals {
   }
 }
 
-resource "aws_resourcegroups_group" "resource_group" {
-  for_each = local.environments
-
-  name = "morphlow-${each.key}"
-  resource_query {
-    query = <<JSON
-{
-  "ResourceTypeFilters": [
-    "AWS::AllSupported"
-  ],
-  "TagFilters": [
-    {
-      "Key": "Project",
-      "Values": [ "morphlow" ]
-    },
-    {
-      "Key": "Environment",
-      "Values": [ "${each.key}" ]
-    }
-  ]
-}
-JSON
-  }
-}
-
 # resource "aws_ecs_cluster" "cluster" {
 #   name = "morphlow_ecs_cluster"
 
