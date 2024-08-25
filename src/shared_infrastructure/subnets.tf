10c4752976706aa49aae6d9339fd925eada2795b
Tripp Shelnutt <trippshelnutt@users.noreply.github.com>
2024-08-24 22:51:32 -0400

    public subnets (#16)

--- src/shared_infrastructure/subnets.tf
+++ src/shared_infrastructure/subnets.tf
@@ -1,28 +1,30 @@
-resource "aws_subnet" "private_subnet_1" {
-  for_each = local.environments
-
-  vpc_id            = aws_vpc.vpc[each.key].id
-  cidr_block        = each.value.subnet_ips[0]
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name        = "${each.key}_private_subnet_1"
-    Project     = "morphlow"
-    Environment = "${each.key}"
-  }
+locals {
+  all_subnets = flatten([
+    for env_key, env in local.environments : [
+      for subnet_key, subnet in env.subnets : {
+        env_key           = env_key
+        subnet_key        = subnet_key
+        vpc_id            = aws_vpc.vpc[env_key].id
+        cidr_block        = subnet.cidr_block
+        name              = "${env_key}_${subnet.name}"
+        availability_zone = subnet.availability_zone
+      }
+    ]
+  ])
 }
 
+resource "aws_subnet" "subnet" {
+  for_each = tomap({
+    for subnet in local.all_subnets : "${subnet.env_key}.${subnet.subnet_key}" => subnet
+  })
 
-resource "aws_subnet" "private_subnet_2" {
-  for_each = local.environments
-
-  vpc_id            = aws_vpc.vpc[each.key].id
-  cidr_block        = each.value.subnet_ips[1]
-  availability_zone = "us-east-1b"
+  vpc_id            = each.value.vpc_id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
 
   tags = {
-    Name        = "${each.key}_private_subnet_2"
+    Name        = each.value.name
     Project     = "morphlow"
-    Environment = "${each.key}"
+    Environment = each.value.env_key
   }
 }
