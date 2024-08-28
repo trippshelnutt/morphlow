00a2282e470cd3d53a2fc35d9e317b11be9c05ea
Tripp Shelnutt <trippshelnutt@outlook.com>
2024-08-27 22:04:21 -0400

    added client app

--- src/shared_infrastructure/service.tf
+++ src/shared_infrastructure/service.tf
@@ -24,3 +24,30 @@ resource "aws_ecs_service" "api_service" {
 
   depends_on = [aws_lb_listener.https_listener]
 }
+
+resource "aws_ecs_service" "app_service" {
+  for_each = local.environments
+
+  name            = "${each.key}_app_service"
+  cluster         = aws_ecs_cluster.app_cluster[each.key].id
+  task_definition = aws_ecs_task_definition.app_task[each.key].arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  network_configuration {
+    subnets          = [for key, subnet in aws_subnet.private_subnet : subnet.id if startswith(key, each.key)]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_blue_target_group[each.key].arn
+    container_name   = "morphlow-client"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.https_listener]
+}
--- src/shared_infrastructure/task_definition.tf
+++ src/shared_infrastructure/task_definition.tf
@@ -28,3 +28,34 @@ resource "aws_ecs_task_definition" "api_task" {
     Environment = "${each.key}"
   }
 }
+
+resource "aws_ecs_task_definition" "app_task" {
+  for_each = local.environments
+
+  family                   = "${each.key}_morphlow_client"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  task_role_arn            = data.aws_iam_role.morphlow_task_role.arn
+  execution_role_arn       = data.aws_iam_role.morphlow_execution_role.arn
+  container_definitions = jsonencode(
+    [
+      {
+        "name" : "morphlow-client",
+        "image" : "${data.aws_ecr_repository.morphlow_client_repository.repository_url}",
+        "essential" : true,
+        "portMappings" : [{
+          "protocol" : "tcp",
+          "hostPort" : 8080,
+          "containerPort" : 8080
+        }]
+      }
+    ]
+  )
+
+  tags = {
+    Project     = "morphlow"
+    Environment = "${each.key}"
+  }
+}
